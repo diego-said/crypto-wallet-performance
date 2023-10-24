7894c22f166ab0c7c86316a5e19ede478199b6bb
Diego Said <diego@doublelogic.com.br>
2023-10-24 12:17:11 -0300

    Project base

---
+++ src/main/java/br/com/doublelogic/cryptowalletperformance/CryptoWalletPerformanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.doublelogic.cryptowalletperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWalletPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWalletPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/doublelogic/cryptowalletperformance/CryptoWalletPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doublelogic.cryptowalletperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWalletPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
